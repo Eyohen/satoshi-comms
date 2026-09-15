ef88b4735a699d62a9fd66d76f3a667dc773ed76
Eyohen <henry.eyo2@gmail.com>
2026-09-15 16:39:19 +0100

    freshfold

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Work from './pages/Work';
+import WorkIndex from './pages/WorkIndex';
 import InfoPage from './pages/InfoPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/work" element={<Work />} />
+      <Route path="/work" element={<WorkIndex />} />
+      <Route path="/work/jeets-and-cream" element={<Work />} />
+      <Route path="/work/freshfold" element={<Work />} />
       <Route path="/why-us" element={<InfoPage />} />
       <Route path="/process" element={<InfoPage />} />
       <Route path="/contact" element={<InfoPage />} />
